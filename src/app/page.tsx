06dfb2f720a292d56d343bad7802e47356083162
AChangXD <markchangan@gmail.com>
2023-07-30 12:50:09 -0400

    Added model header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
-import Drawer from '@mui/material/Drawer';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
+
 import ModelCard from '@/components/ModelCard';
-import { Button } from '@mui/material';
+
 import AddButton from '@/components/SearchBar';
 import { headers } from 'next/headers';
+import ModelHeader from '@/components/ModelHeader';
 
 export default async function HomePage() {
   // Fetch all model information on the server:
@@ -23,30 +19,19 @@ export default async function HomePage() {
   });
   const resBody = await res.json();
 
-  // const res = await fetch('/api/model-info', { method: 'GET' });
-  console.log(resBody);
+  console.log(resBody.data);
 
   return (
     <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
-      <Box>
-        <Typography>Test</Typography>
-      </Box>
-      <Grid
-        container
-        spacing={2}
-      >
-        <Grid
-          xs={12}
-          md={6}
-          lg={4}
-          sx={{ width: '100%' }}
-        >
-          <ModelCard
-            heading="test"
-            text="abcd"
-          />
-        </Grid>
-      </Grid>
+      {/* Header */}
+      <ModelHeader numberOfModels={255555} />
+
+      {/* List of models */}
+
+      <ModelCard
+        heading="test"
+        text="abcd"
+      />
     </Box>
   );
 }
---
+++ src/components/ModelHeader.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import {
+  Box,
+  Button,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+
+const FILTER_OPTIONS = ['Ascending', 'Descending', 'Likes'];
+
+export default function ModelHeader({
+  numberOfModels,
+}: {
+  numberOfModels: number;
+}) {
+  /* -------------------------------------------------------------------------- */
+  /*                                   States                                   */
+  /* -------------------------------------------------------------------------- */
+  const [filterMode, setFilterMode] = useState(FILTER_OPTIONS[0]);
+
+  /* -------------------------------------------------------------------------- */
+  /*                                 JSX Return                                 */
+  /* -------------------------------------------------------------------------- */
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        mt: 1,
+        mb: 2,
+      }}
+    >
+      <Box
+        sx={{
+          wxidth: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 3,
+          alignItems: 'center',
+        }}
+      >
+        <Typography
+          variant="h6"
+          fontWeight="bold"
+        >
+          Models
+        </Typography>
+        <Typography variant="h6">{numberOfModels}</Typography>
+        {/* TODO: Switch to Autocomplete after backend is setup: */}
+        <TextField></TextField>
+      </Box>
+      <Box>
+        <Select
+          value={filterMode}
+          onChange={(e: SelectChangeEvent) => {
+            setFilterMode(e.target.value);
+          }}
+        >
+          {FILTER_OPTIONS.map((option) => {
+            return <MenuItem value={option}>{option}</MenuItem>;
+          })}
+        </Select>
+      </Box>
+    </Box>
+  );
+}
